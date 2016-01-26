b00ac5b2e098c90b998e7555f81bcfa9ea374aed
Stein Tore Tøsse <stein.tore@kodemaker.no>
2016-01-26 08:12:55 +0100

    Round the temperature

--- components/TemperatureItem.jsx
+++ components/TemperatureItem.jsx
@@ -5,7 +5,7 @@ const TemperatureItem = React.createClass({
     render: function () {
         return (<div className="item">
             <Name text={this.props.label} value={this.props.data.item.state} icon={this.props.data.icon}/>
-            <div className="tempType">{this.props.data.item.state == 'Uninitialized' ? '? ' : this.props.data.item.state} &deg;C</div>
+            <div className="tempType">{this.props.data.item.state == 'Uninitialized' ? '? ' : Math.round(this.props.data.item.state)} &deg;C</div>
         </div>)
     }
 });
